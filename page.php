71872ec0e04e77b8224d8a866151ae9ec7dcf2a7
Nicolas Lagarde <oucelavatil@gmail.com>
2016-05-30 23:56:46 +0200

    Simplified page structure

--- page.php
+++ page.php
@@ -14,30 +14,22 @@
 
 get_header(); ?>
 
-<div class="grid grid-pad solid-background">
+    <main id="main gallery" class="site-main page" role="main">
+    
+        <?php while ( have_posts() ) : the_post(); ?>
 
-        <div id="primary" class="content-area">
-            <main id="main" class="site-main" role="main">
-    
-                <?php while ( have_posts() ) : the_post(); ?>
-    
-                    <?php get_template_part( 'template-parts/content', 'page' ); ?>
-    
-                    <?php
-                        // If comments are open or we have at least one comment, load up the comment template.
-                        if ( comments_open() || get_comments_number() ) :
-                            comments_template();
-                        endif;
-                    ?>
-    
-                <?php endwhile; // End of the loop. ?>
-    
-            </main><!-- #main -->
-        </div><!-- #primary -->
-        
-    
-<?php get_sidebar(); ?>
+            <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-</div><!-- grid -->
+            <?php
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+            ?>
+
+        <?php endwhile; // End of the loop. ?>
+    
+    </main>
+
 
 <?php get_footer(); ?>
