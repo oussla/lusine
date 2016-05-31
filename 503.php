db859a2f630affbfdc5f4bdc263bf70b8b1497eb
Nicolas Lagarde <oucelavatil@gmail.com>
2016-06-01 00:10:20 +0200

    Added 503 maintenance page

---
+++ 503.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>L'Usine Tattoo</title>
+</head>
+<body>
+<!-- L'USINE TATTOO - MAINTENANCE MODE -->
+<center>
+	<a href="https://www.facebook.com/lusinetattoo4000/">
+		<img src="/static/soon.jpg" alt="L'Usine" style="width: 100%; max-width: 500px;" />
+	</a>
+</center>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-4531688-34', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+</html>
