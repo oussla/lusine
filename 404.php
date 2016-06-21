88dcc8eb9067a9e2cf62346ad5d64b5c5d2b25b9
Nicolas Lagarde <oucelavatil@gmail.com>
2016-06-21 22:23:22 +0200

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+
+get_header(); ?>
+
+    <main id="main" class="site-main page" role="main">
+    
+
+		<article>
+			<header class="entry-header">
+				<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<p></p>
+
+		</article><!-- #post-## -->
+
+    
+    </main>
+
+
+<?php get_footer(); 
